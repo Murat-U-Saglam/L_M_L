84fb85ef7ebac015e1b9f5766379dfdedc007d84
Murat Saglam <muratusaglam@gmail.com>
2023-07-07 17:19:39 +0100

    Need 2 clean data

--- MoneyBrinterBRR/setup.sh
+++ MoneyBrinterBRR/setup.sh
@@ -2,3 +2,4 @@ kaggle competitions download -c g-research-crypto-forecasting
 mkdir data
 unzip g-research-crypto-forecasting.zip -d data
 rm g-research-crypto-forecasting.zip
+mkdir ../models/MoneyBrinterBRR
